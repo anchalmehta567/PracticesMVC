be01b1677f6e8ce124a4144725d93169b64d1a77
Aanchal Mehta <aanchal.mehta@nagarro.com>
2021-08-24 10:34:46 +0530

    Adding Validations

---
+++ MVCPractice/Controllers/ValidationsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCPractice.Models;
+
+namespace MVCPractice.Controllers
+{
+    public class ValidationsController : Controller
+    {
+        // GET: Validations
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SubmitData(DataEmployee emp) 
+        {
+            if (ModelState.IsValid) 
+            {
+                return View();
+            }
+            return View("Index");
+        }
+        public ActionResult CustomValidation() 
+        { 
+            return View(); 
+        }
+        [HttpPost]
+        public ActionResult CustomValidation(DataEmployee emp)
+        {
+            if (ModelState.IsValid) 
+            {
+                ModelState.Clear();
+                return View();
+            }
+            return View();
+        }
+
+    }
+}
---
+++ MVCPractice/EmployeeeDataCustomValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace MVCPractice
+{
+    public class EmployeeeDataCustomValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null) 
+            { 
+                string Valueinput = value.ToString();
+                if (string.IsNullOrEmpty(Valueinput)) 
+                {
+                    return ValidationResult.Success;
+                }
+                if (Valueinput.Contains("Anchal")) 
+                {
+                    return ValidationResult.Success;
+                }
+             
+            }
+            return new ValidationResult("String Should Contains Ancahl");
+        }
+    }
+}
