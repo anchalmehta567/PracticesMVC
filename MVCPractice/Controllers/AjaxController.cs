d5cd9c0f85928847aa97a74a0f8f3e8b1bd63428
Aanchal Mehta <aanchal.mehta@nagarro.com>
2021-08-25 10:24:32 +0530

    Basic of Ajax

---
+++ MVCPractice/Controllers/AjaxController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCPractice.Models;
+using Newtonsoft.Json;
+namespace MVCPractice.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetStudent()
+        {
+            StudentData std = new StudentData()
+            {
+                id = 2,
+                Email = "aanchalmehta@gmail.com",
+                name = "Anchal"
+            };
+            var json = JsonConvert.SerializeObject(std);
+
+            return Json(json,JsonRequestBehavior.AllowGet); 
+        }
+        [HttpPost]
+        public JsonResult AddStudent(StudentData student) 
+        {
+            return Json("true", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MVCPractice/Models/StudentData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCPractice.Models
+{
+    public class StudentData
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string Email { get; set; }
+    }
+}
