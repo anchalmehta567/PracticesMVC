909d123d4375b51713a2f213a98c9504acc8bb91
Aanchal Mehta <aanchal.mehta@nagarro.com>
2021-09-02 10:51:45 +0530

    framework Identity

---
+++ IdentityFramework/IdentityFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityFramework.Startup))]
+namespace IdentityFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ MVCPractice/Areas/Sales/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractice.Areas.Sales
+{
+    public class HomeController : Controller
+    {
+        // GET: Sales/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
