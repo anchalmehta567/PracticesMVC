87b991e62e53c5afdf0edce56aa17e9cf1e6dfc6
Aanchal Mehta <aanchal.mehta@nagarro.com>
2021-10-18 15:16:15 +0530

    Facade  Desgin Pattern

---
+++ FacadeDesginPatternDemo/FacadeDesginPatternDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FacadeDesginPatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------CLIENT ORDERS FOR PIZZA----------------------------\n");
+            var facadeForClient = new RestaurantFacade();
+            facadeForClient.GetNonVegPizza();
+            facadeForClient.GetVegPizza();
+            Console.WriteLine("\n----------------------CLIENT ORDERS FOR BREAD----------------------------\n");
+            facadeForClient.GetGarlicBread();
+            facadeForClient.GetCheesyGarlicBread();
+        }
+    }
+}
