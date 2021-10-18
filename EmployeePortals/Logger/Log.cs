9293d3e204539a5bd8ea4d9df89263129a76cd33
Aanchal Mehta <aanchal.mehta@nagarro.com>
2021-10-18 09:05:07 +0530

    Exception Logging using Singletone design pattern

---
+++ EmployeePortals/Logger/Log.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+       public sealed class Log:ILog
+        {
+         private Log()
+        {
+        }
+        private static readonly Lazy<Log> instance = new Lazy<Log>(() => new Log());
+
+
+        public static Log GetInstance
+            {
+                get
+                {
+                    return instance.Value;
+                }
+            }
+
+        public void LogException(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
+
