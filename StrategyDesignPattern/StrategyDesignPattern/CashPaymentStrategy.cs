5ef79c3535d8d4ec08e64989ee5f30f987b44f02
Aanchal Mehta <aanchal.mehta@nagarro.com>
2021-10-20 09:23:54 +0530

    startegy  Desgin Pattern

---
+++ StrategyDesignPattern/StrategyDesignPattern/CashPaymentStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyDesignPattern
+{
+    public class CashPaymentStrategy : IPaymentStrategy
+    {
+        public void Pay(double amount)
+        {
+            Console.WriteLine("Customer pays Rs " + amount + " By Cash");
+        }
+    }
+}
---
+++ StrategyDesignPattern/StrategyDesignPattern/PaymentContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyDesignPattern
+{
+    public class PaymentContext
+    {
+        private IPaymentStrategy PaymentStrategy;
+      
+        public void SetPaymentStrategy(IPaymentStrategy strategy)
+        {
+            this.PaymentStrategy = strategy;
+        }
+        public void Pay(double amount)
+        {
+            PaymentStrategy.Pay(amount);
+        }
+    }
+}
